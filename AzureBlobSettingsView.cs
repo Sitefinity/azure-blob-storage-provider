29f2ecdede13438f020d5e635a1c78caf8b3b51f
bonny-bonev <bonny.bonev@telerik.com>
2015-09-12 15:13:19 +0300

    Azure blob storage provider moved to a separate project
    The provider is now separated from the Sitefinity project.

---
+++ AzureBlobSettingsView.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web.UI.WebControls;
+using Telerik.Sitefinity.Web.UI;
+using Telerik.Sitefinity.Web.UI.Fields;
+using System.Web;
+using System.Web.UI;
+using Telerik.Sitefinity.Modules.Libraries.Web.UI.BlobStorage;
+
+namespace Telerik.Sitefinity.Azure.BlobStorage
+{
+    /// <summary>
+    /// Basic settings wizard view for configuring the connection to Azure blob storage
+    /// </summary>
+    /// <remarks>
+    /// Expected provider parameters connectionString, containerName
+    /// </remarks>
+    public class AzureBlobSettingsView : AjaxDialogBase, IBlobSettingsView
+    {
+
+        /// <summary>
+        /// Gets or sets the path of the external template to be used by the control.
+        /// </summary>
+        /// <value></value>
+        public override string LayoutTemplatePath
+        {
+            get
+            {
+                return layoutTemplatePath;
+            }
+            set
+            {
+                return;
+            }
+        }
+
+        protected override string LayoutTemplateName
+        {
+            get { return null; }
+        }
+
+
+        /// <summary>
+        /// Gets the name of the account UI control.
+        /// </summary>
+        /// <value>The name of the account.</value>
+        public TextField AccountName
+        {
+            get
+            {
+                return this.Container.GetControl<TextField>("accountName", true);
+            }
+        }
+
+        /// <summary>
+        /// Gets the account key UI control.
+        /// </summary>
+        /// <value>The account key.</value>
+        public TextField AccountKey
+        {
+            get
+            {
+                return this.Container.GetControl<TextField>("accountKey", true);
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the container UI control.
+        /// </summary>
+        /// <value>The name of the container.</value>
+        public TextField ContainerName
+        {
+            get
+            {
+                return this.Container.GetControl<TextField>("containerName", true);
+            }
+        }
+
+        /// <summary>
+        /// Gets the public host UI control.
+        /// </summary>
+        public TextField PublicHost
+        {
+            get
+            {
+                return this.Container.GetControl<TextField>("publicHost", true);
+            }
+        }
+
+        /// <summary>
+        /// Gets the default endpoints protocol UI control.
+        /// </summary>
+        /// <value>The default endpoints protocol.</value>
+        public CheckBox DefaultEndpointsProtocol
+        {
+            get
+            {
+                return this.Container.GetControl<CheckBox>("defaultEndpointsProtocol", true);
+            }
+        }
+
+        public CheckBox UseDevStorage
+        {
+            get
+            {
+                return this.Container.GetControl<CheckBox>("useDevStorage", true);
+            }
+        }
+
+        /// <summary>
+        /// Initializes the settings view controls.
+        /// </summary>
+        /// <param name="container"></param>
+        protected override void InitializeControls(GenericContainer container)
+        {
+            if (!this.Page.IsPostBack)
+            {
+                if (settings[AzureBlobStorageProvider.ConnectionStringKey] != null)
+                {
+                    PopulateUIFromConnectionString(
+                        settings[AzureBlobStorageProvider.ConnectionStringKey],
+                        settings[AzureBlobStorageProvider.SharedAccessSignatureKey]);
+                }
+
+                if (settings[AzureBlobStorageProvider.ContainerNameKey] != null)
+                    this.ContainerName.Value = settings[AzureBlobStorageProvider.ContainerNameKey];
+
+                if (settings[AzureBlobStorageProvider.PublicHostKey] != null)
+                    this.PublicHost.Value = settings[AzureBlobStorageProvider.PublicHostKey];
+            }
+        }
+
+        protected override HtmlTextWriterTag TagKey
+        {
+            get
+            {
+                //we use div wrapper tag to make easier common styling
+                return HtmlTextWriterTag.Div;
+            }
+        }
+
+        /// <summary>
+        /// Gets the azure connection string from the UI Controls
+        /// </summary>
+        /// <returns></returns>
+        private string GetAzureConnectionStringFromUI()
+        {
+            if (UseDevStorage.Checked)
+            {
+                return devStoreConnectionString;
+            }
+
+            string protocol = DefaultEndpointsProtocol.Checked ? "https" : "http";
+            string accountName = AccountName.Value.ToString();
+            string key = AccountKey.Value.ToString();
+
+            var signature = GetSignature(key);
+            if (signature != null)
+            {
+                return string.Format("BlobEndpoint={0}://{1}.blob.core.windows.net/;SharedAccessSignature={2}",
+                    protocol, accountName, signature);
+            }
+
+            return string.Format("DefaultEndpointsProtocol={0};AccountName={1};AccountKey={2}",
+                protocol, accountName, key);    
+        }
+
+        private string GetSignature(string key)
+        {
+            var sasMatch = sasRegex.Match(key);
+            if (!sasMatch.Success)
+                return null;
+
+            return HttpUtility.UrlDecode(sasMatch.Groups["sig"].Value);
+        }
+
+        /// <summary>
+        /// Populates the UI controls from connection string.
+        /// </summary>
+        /// <param name="azureConnectionString">The azure connection string.</param>
+        private void PopulateUIFromConnectionString(string azureConnectionString, string sas = null)
+        {
+            if (azureConnectionString == devStoreConnectionString)
+            {
+                this.UseDevStorage.Checked = true;
+                return;
+            }
+            
+            var settings = azureConnectionString.Split(';');
+            foreach (var setting in settings)
+            {
+                var settingsPair = setting.Split(new char[] {'='}, 2, StringSplitOptions.None);
+                string value = settingsPair[1];
+
+                switch (settingsPair[0])
+                {
+                    case "AccountName":
+                        this.AccountName.Value = value;
+                        break;
+                    case "AccountKey":
+                        this.AccountKey.Value = value;
+                        break;
+                    case "DefaultEndpointsProtocol":
+                        this.DefaultEndpointsProtocol.Checked = value.ToLowerInvariant() == "https";
+                        break;
+                    case "BlobEndpoint":
+                        var uri = new Uri(value);
+                        this.DefaultEndpointsProtocol.Checked = uri.Scheme == "https";
+                        this.AccountName.Value = uri.Host.Substring(0, uri.Host.IndexOf('.'));
+                        break;
+                    case "SharedAccessSignature":
+                        if (sas == null)
+                            this.AccountKey.Value = string.Format("?sig={0}", HttpUtility.UrlEncode(value));
+                        break;
+                }
+            }
+
+            if (sas != null)
+                this.AccountKey.Value = sas;
+        }
+
+        /// <summary>
+        /// Gets or sets the blob storage settings.
+        /// </summary>
+        /// <value>The settings.</value>
+        /// <remarks>
+        /// Expected provider parameters connectionString, containerName
+        /// </remarks>
+        public NameValueCollection Settings
+        {
+            get
+            {
+                var key = this.AccountKey.Value.ToString();
+                var sas = GetSignature(key) == null ? null : key;
+
+                return new NameValueCollection()
+                {
+                    {
+                        AzureBlobStorageProvider.ConnectionStringKey,
+                        GetAzureConnectionStringFromUI() 
+                    },
+                    {
+                        AzureBlobStorageProvider.ContainerNameKey,
+                        this.ContainerName.Value.ToString()
+                    },
+                    {
+                        AzureBlobStorageProvider.SharedAccessSignatureKey,
+                        sas
+                    },
+                    {
+                        AzureBlobStorageProvider.PublicHostKey,
+                        this.PublicHost.Value.ToString()
+                    }
+                };
+            }
+            set
+            {
+                settings = value;
+            }
+        }
+
+        private NameValueCollection settings;        
+
+        private const string devStoreConnectionString = "UseDevelopmentStorage=true";
+        private static readonly Regex sasRegex = new Regex(@"sig=(?<sig>[a-zA-Z0-9\+/%]+[=]{0,2})", RegexOptions.Compiled);
+
+        public static readonly string layoutTemplatePath = ControlUtilities.ToVppPath("Telerik.Sitefinity.Resources.Templates.Backend.Configuration.Basic.AzureBlobSettingsView.ascx");
+        //public static readonly string layoutTemplatePath = "~/AzureBlobSettingsView.ascx";        
+    }
+}
---
+++ AzureBlobStorageProvider.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Telerik.Sitefinity.BlobStorage;
+using Telerik.Sitefinity.Modules.Libraries.BlobStorage;
+using Telerik.Sitefinity.Web;
+using Storage = Microsoft.WindowsAzure.Storage;
+
+namespace Telerik.Sitefinity.Azure.BlobStorage
+{
+    /// <summary>
+    /// Implements a logic for persisting BLOB data in Azure Blob Storage
+    /// </summary>
+    public class AzureBlobStorageProvider : CloudBlobStorageProvider
+    {
+        #region Properties
+
+        public const string ConnectionStringKey = "connectionString";
+        public const string ContainerNameKey = "containerName";
+        public const string PublicHostKey = "publicHost";
+        public const string SharedAccessSignatureKey = "SAS";
+        public const string ParallelOperationThreadCountKey = "parallelOperationThreadCount";
+
+        #endregion
+
+        #region Initialization
+        /// <summary>
+        /// Initializes the storage.
+        /// </summary>
+        /// <param name="config">The config.</param>
+        protected override void InitializeStorage(NameValueCollection config)
+        {
+            this.client = this.CreateBlobClient(config);
+
+            this.containerName = config[ContainerNameKey];
+            if (String.IsNullOrEmpty(containerName))
+                containerName = this.Name.ToLower();
+            // TODO: Azure - container name should be validated 
+
+            config.Remove(ContainerNameKey);
+
+            this.rootUrl = UrlPath.GetAbsoluteHost(config[PublicHostKey])
+                ?? this.client.BaseUri.ToString();
+            if (this.rootUrl[this.rootUrl.Length - 1] != '/')
+                this.rootUrl += "/";
+        }
+
+        protected virtual CloudBlobClient CreateBlobClient(NameValueCollection config)
+        {
+            this.connectionString = config[ConnectionStringKey];
+            if (String.IsNullOrEmpty(this.connectionString))
+                throw new ConfigurationErrorsException("'{0}' is not specified.".Arrange(ConnectionStringKey));
+            config.Remove(ConnectionStringKey);
+            CloudStorageAccount account = null;
+            try
+            {
+                account = CloudStorageAccount.Parse(this.connectionString);
+            }
+            catch (FormatException e)
+            {
+                throw new BlobStorageException("Invalid blob storage credentials", e);
+            }
+
+            string sas = config[SharedAccessSignatureKey];
+            if (sas != null)
+            {
+                config.Remove(SharedAccessSignatureKey);
+                return new CloudBlobClient(account.BlobEndpoint, new StorageCredentials(sas));
+            }
+
+            var blobClient = account.CreateCloudBlobClient();
+
+            var val = config[ParallelOperationThreadCountKey];
+            if (!String.IsNullOrEmpty(val))
+                blobClient.DefaultRequestOptions.ParallelOperationThreadCount = int.Parse(val);
+            config.Remove(ParallelOperationThreadCountKey);
+
+            return blobClient;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Gets the upload stream.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <returns></returns>
+        public override Stream GetUploadStream(IBlobContent content)
+        {
+            var container = this.GetOrCreateContainer(this.containerName);
+            CloudBlockBlob blob = container.GetBlockBlobReference(GetBlobName(content));
+            return blob.OpenWrite();
+        }
+
+        /// <summary>
+        /// Gets the download stream.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <returns></returns>
+        public override Stream GetDownloadStream(IBlobContent content)
+        {
+            CloudBlob blob = this.GetBlob(content);
+            return blob.OpenRead();
+        }
+
+        public void DownloadToStream(IBlobContent content, Stream target)
+        {
+            CloudBlob blob = this.GetBlob(content);
+            blob.DownloadToStream(target);
+        }
+
+        /// <inheritdoc />
+        public override void Delete(IBlobContentLocation content)
+        {
+            CloudBlob blob = this.GetBlob(content);
+            try
+            {
+                var requestOptions = new Storage.Blob.BlobRequestOptions()
+                {
+                    RetryPolicy = new Storage.RetryPolicies.NoRetry() // RetryPolicies.Retry(1, TimeSpan.FromSeconds(1))
+                };
+                blob.DeleteIfExists(DeleteSnapshotsOption.None, null, requestOptions, null);
+            }
+            catch (Exception e)
+            {
+                throw new BlobStorageException(String.Format("Cannot delete BLOB '{0}' from library storage '{1}'. {2}: {3}.", this.GetBlobName(content), this.Name, e.Source, e.Message), e);
+            }
+        }
+
+        /// <inheritdoc />
+        public override bool BlobExists(IBlobContentLocation location)
+        {
+            CloudBlob blob = this.GetBlob(location);
+            return blob.Exists();
+        }
+
+        /// <inheritdoc />
+        public override void SetProperties(IBlobContentLocation content, IBlobProperties properties)
+        {
+            var blob = this.GetBlob(content);
+
+            blob.Properties.ContentType = properties.ContentType;
+            blob.Properties.CacheControl = properties.CacheControl;
+
+            blob.BeginSetProperties(null, null);
+        }
+
+        /// <inheritdoc />
+        public override IBlobProperties GetProperties(IBlobContentLocation location)
+        {
+            var blob = this.GetBlob(location);
+            blob.FetchAttributes();
+
+            return new Telerik.Sitefinity.Modules.Libraries.BlobStorage.BlobProperties
+            {
+                ContentType = blob.Properties.ContentType,
+                CacheControl = blob.Properties.CacheControl
+            };
+        }
+
+        /// <inheritdoc />
+        public override string GetItemUrl(IBlobContentLocation content)
+        {
+            return string.Concat(this.rootUrl, this.GetBlobPath(content));
+        }
+
+        /// <inheritdoc />
+        public override void Copy(IBlobContentLocation source, IBlobContentLocation destination)
+        {
+            var sourceBlob = this.GetBlob(source);
+            var destBlob = this.GetBlob(destination);
+
+            if (!(this.client.Credentials is StorageCredentials))
+            {
+                using (var uploadStream = sourceBlob.OpenRead())
+                {
+                    destBlob.UploadFromStream(uploadStream);
+                }
+            }
+            else
+            {
+                // This is a workaround for the SAS authorization case, for which CopyFromBlob throws an exception.
+                // It imposes PERFORMANCE, NETWORK and FINANCIAL costs. :(
+                using (var uploadSteam = destBlob.OpenWrite())
+                    sourceBlob.DownloadToStream(uploadSteam);
+
+                this.SetProperties(destination, this.GetProperties(source));
+            }
+        }
+
+        /// <inheritdoc />
+        public override void Move(IBlobContentLocation source, IBlobContentLocation destination)
+        {
+            this.Copy(source, destination);
+            var sourceBlob = this.GetBlob(source);
+            sourceBlob.BeginDelete(null, null);
+        }
+
+        /// <inheritdoc />
+        public override bool HasSameLocation(BlobStorageProvider other)
+        {
+            var otherAureProvider = other as AzureBlobStorageProvider;
+            if (otherAureProvider != null)
+                return this.connectionString.Equals(otherAureProvider.connectionString, StringComparison.OrdinalIgnoreCase) &&
+                    this.containerName.Equals(otherAureProvider.containerName, StringComparison.OrdinalIgnoreCase) &&
+                    this.rootUrl.Equals(otherAureProvider.rootUrl, StringComparison.OrdinalIgnoreCase);
+
+            return base.HasSameLocation(other);
+        }
+
+        #region Private
+
+        private string GetBlobPath(IBlobContentLocation content)
+        {
+            return String.Concat(this.containerName, "/", this.GetBlobName(content));
+        }
+
+        private CloudBlockBlob GetBlob(IBlobContentLocation blobLocation)
+        {
+            var container = this.client.GetContainerReference(this.containerName);
+            var blob = container.GetBlockBlobReference(this.GetBlobName(blobLocation));
+            return blob;
+        }
+
+        private CloudBlobContainer GetOrCreateContainer(string name)
+        {
+            CloudBlobContainer container = this.client.GetContainerReference(name);
+
+            // Note: For some reason this returns false even the container did not already exists (even in the 5.0.2 this behavior is same)
+            container.CreateIfNotExists(BlobContainerPublicAccessType.Blob);
+            return container;
+        }
+
+        #endregion
+
+        #region Fields
+
+        private string connectionString;
+        private CloudBlobClient client;
+        private string rootUrl;
+        private string containerName;
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Implements a wrapper for Azure Blob Sream to fix an issue with CloudBlob.OpenRead() not returning the whole data
+    /// </summary>
+    internal class AzureBlobDownloadStream : Stream
+    {
+        public AzureBlobDownloadStream(CloudBlobStream stream)
+        {
+            this.originalStream = stream;
+        }
+
+        public override bool CanRead
+        {
+            get { return originalStream.CanRead; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return originalStream.CanSeek; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return originalStream.CanWrite; }
+        }
+
+        public override void Flush()
+        {
+            originalStream.Flush();
+        }
+
+        public override long Length
+        {
+            get { return originalStream.Length; }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                return this.originalStream.Position;
+            }
+            set
+            {
+                this.originalStream.Position = value;
+            }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var bytesRead = this.originalStream.Read(buffer, offset, count);
+            if (offset == 0 && bytesRead > 0 && bytesRead < count)
+            {
+                var bytesLeft = count - bytesRead;
+                var extendedBuffer = new byte[bytesLeft];
+                var bytesLeftRead = this.originalStream.Read(extendedBuffer, 0, bytesLeft);
+
+                if (bytesLeftRead > 0)
+                {
+                    Array.Copy(extendedBuffer, 0, buffer, bytesRead, bytesLeftRead);
+                    bytesRead += bytesLeftRead;
+                }
+            }
+
+            return bytesRead;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return this.originalStream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            this.originalStream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            this.originalStream.Write(buffer, offset, count);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            originalStream.Dispose();
+        }
+
+        private readonly CloudBlobStream originalStream;
+    }
+}
