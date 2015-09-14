fd9c7ce3dda8c9858a6263d8a0f017f4db351f7d
bonny-bonev <bonny.bonev@telerik.com>
2015-09-14 14:58:53 +0300

    Added initialization mechanism
    The provider configuration will be registered automatically

---
+++ Initializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telerik.Sitefinity.Azure
+{
+    public static class Initializer
+    {
+        public static void Initialize()
+        {
+            SystemManager
+        }
+    }
+}
