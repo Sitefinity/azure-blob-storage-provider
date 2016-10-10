b13d78eda63223d26bce031d34381d132e77995f
gg3orgiev <gigeorgiev@progress.com>
2016-10-10 14:16:18 +0300

    Update AssemblyCopyright.

--- Telerik.Sitefinity.Azure/Properties/AssemblyInfo.cs
+++ Telerik.Sitefinity.Azure/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Web;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Telerik.Sitefinity.Azure")]
-[assembly: AssemblyCopyright("Copyright © Telerik 2016")]
+[assembly: AssemblyCopyright("Copyright © 2005-2016 Telerik AD. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
