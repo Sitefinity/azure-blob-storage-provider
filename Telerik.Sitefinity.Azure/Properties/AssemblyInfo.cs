6e9435dd9a235efe0e8e2f14f2a05b9e1792c165
Ivan Dimitrov <ivan.dimitrov@telerik.com>
2018-12-14 15:04:24 +0200

    Update AssemblyInfo.cs

--- Telerik.Sitefinity.Azure/Properties/AssemblyInfo.cs
+++ Telerik.Sitefinity.Azure/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Web;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Telerik.Sitefinity.Azure")]
-[assembly: AssemblyCopyright("Copyright © 2005-2018 Telerik EAD. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2005-2018 Progress Software Corporation and/or one of its subsidiaries or affiliates. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
