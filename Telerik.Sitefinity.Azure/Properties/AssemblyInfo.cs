840a597cda531b94280256981ade3a267b15ee28
Orlin Draganov <draganov@progress.com>
2021-06-18 13:07:31 +0300

    Upgraded assembly info version to 14.0

--- Telerik.Sitefinity.Azure/Properties/AssemblyInfo.cs
+++ Telerik.Sitefinity.Azure/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Web;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("13.3.7600.0")]
-[assembly: AssemblyFileVersion("13.3.7600.0")]
+[assembly: AssemblyVersion("14.0.7700.0")]
+[assembly: AssemblyFileVersion("14.0.7700.0")]
 
 [assembly: PreApplicationStartMethod(typeof(Telerik.Sitefinity.Azure.Initializer), "Initialize")]
